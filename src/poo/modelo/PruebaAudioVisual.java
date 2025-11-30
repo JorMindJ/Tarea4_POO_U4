242172e839ba7fbbd565e862d04e4c6990e071f7
JorMindJ <jmindiola@est.ups.edu.ec>
2025-11-30 10:10:39 -0500

    Sistema de Contenido Audiovisual actualizado a  Código Limpio. - Manejo de Archivos. - Refactorización y Código Limpio. - Aplicación de Principios SOLID. - Implementación del Patrón MVC. - Desarrollo de Pruebas Unitarias.

--- src/poo/modelo/PruebaAudioVisual.java
+++ src/poo/modelo/PruebaAudioVisual.java
@@ -1,4 +1,4 @@
-package poo;
+package poo.modelo;
 import uni1a.*;
 
 public class PruebaAudioVisual {
@@ -7,15 +7,18 @@ public class PruebaAudioVisual {
 
         // Crear instancias de las subclases
         ContenidoAudiovisual[] contenidos = new ContenidoAudiovisual[5];
-        contenidos[0] = new Pelicula("Avatar", 125, "Accion", "20th Century Studios");
+        contenidos[0] = new Pelicula("Avatar", null, 125, "Accion", "20th Century Studios", 0);
         contenidos[1] = new SerieDeTV("Game of Thrones", 60, "Fantasy", 8);
         contenidos[2] = new Documental("Cosmos", 45, "Science", "Astronomy");
         contenidos[3] = new SocialReels("Viaje a Japon", 3, "Exploracion", 10000);
-        contenidos[4] = new Publicidad("Nestlé", 7, "Comparativa", "Endulza la vida, no como otras marcas");
+        contenidos[4] = new Publicidad("Nestle", 7, "Comparativa", "Endulza tu dia y la vida");
 
         // Mostrar los detalles de cada contenido audiovisual
         for (ContenidoAudiovisual contenido : contenidos) {
             contenido.mostrarDetalles();
         }
+        String archivoDestino = "estado_del_sistema.csv";
+        BufferedWriterContenidosVisuales.guardarContenidosEnCSV(contenidos, archivoDestino);
     }
+
 }
